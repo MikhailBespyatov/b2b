82db83698319f809aaf301a0629bef827d7c3af1
Nurbek Zhussip <nzhussip@alfabank.kz>
2022-02-01 11:04:12 +0600

    refactor: update default export

--- src/config/enviroments/index.ts
+++ src/config/enviroments/index.ts
@@ -1,5 +1,7 @@
-export default {
+const configs = {
   MODE_DEV: process.env.REACT_APP_DEV,
   SERVER: process.env.REACT_APP_SERVER,
   PUBLIC_URL: process.env.REACT_APP_PUBLIC_URL
 };
+
+export default configs;
