ca29389089f40063214aeca34bb95cb956a02b48
nurbekzhussip <nurbekzhussip@gmail.com>
2022-04-05 23:02:57 +0500

    refactor: remove constant file

---
+++ src/config/constants/status.constants.ts
@@ -0,0 +1,13 @@
+export const FINAL_ORDER_STATUSES = [
+  'completed',
+  'final_merchant_unavaliable',
+  'ready_delivery',
+  'delivered',
+  'return',
+  'part_return',
+  'canceled'
+];
+
+export const ORDER_STATUS = {
+  CANCELED: 'canceled'
+};
