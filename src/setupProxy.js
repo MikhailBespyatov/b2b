fedbe6b5d69849104c8b0054b32d5659f3eea11b
nurbekzhussip <nurbekzhussip@gmail.com>
2022-04-11 17:39:52 +0500

    fix: configs setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const host = 'https://rancher-test.alfa-bank.kz:30001/';
 
 module.exports = function (app) {
   app.use(
     '/services-ui/api/get-organizations',
     createProxyMiddleware({
-      target: 'https://rancher-test.alfa-bank.kz:30001',
-      changeOrigin: true
+      target: host,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/proxy': ''
+      }
     })
   );
 };
