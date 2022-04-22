8965b14ce30d779b337a6513d723a0bfa55052d2
nurbekzhussip <nurbekzhussip@gmail.com>
2022-04-22 07:53:08 +0500

    refactor: add proxy

--- server.js
+++ server.js
@@ -8,31 +8,35 @@ app.use(bodyParser.json());
 
 const root = require('path').join(__dirname, 'build');
 
-app.use(express.static(root));
+app.get(
+  '/api/ibk/*',
+  createProxyMiddleware({
+    target: 'https://rancher-test.alfa-bank.kz:30001/',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/ibk': '/'
+    }
+  })
+);
 
-// app.get(
-//   '/api/ibk/*',
-//   createProxyMiddleware({
-//     target: 'https://rancher-test.alfa-bank.kz:30001/',
-//     changeOrigin: true,
-//     pathRewrite: {
-//       '^/api/ibk': '/'
-//     }
-//   })
-// );
+app.get(
+  '/api/*',
+  createProxyMiddleware({
+    target: 'https://rancher-test.alfa-bank.kz:30380/',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '/'
+    }
+  })
+);
+app.disable('x-powered-by');
+app.set('port', 8080);
+app.use(bodyParser({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.get(
-//   '/api/*',
-//   createProxyMiddleware({
-//     target: 'https://rancher-test.alfa-bank.kz:30380/',
-//     changeOrigin: true,
-//     pathRewrite: {
-//       '^/api': '/'
-//     }
-//   })
-// );
-
+app.use('/', express.static(root));
 app.get('*', (req, res) => {
-  res.sendFile('index.html', { root });
+  res.sendFile('index.html');
 });
 app.listen(process.env.PORT || 3000);
